83799ab4c1de2940d5052532a046dc0aabc60d68
masterfabela <fabelaretorcida@gmail.com>
2020-02-11 16:23:04 +0100

    Terminado, faltaria animaciones

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,13 +1,51 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {
+  state,
+  style,
+  transition,
+  animate,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          width: '240px',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          width: '70px',
+        })
+      ),
+      transition('open <=> closed', [animate('1s')]),
+    ]),
+    trigger('openCloseContent', [
+      state(
+        'open',
+        style({
+          marginLeft: '240px',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          marginLeft: '70px',
+        })
+      ),
+      transition('open <=> closed', [animate('1s')]),
+    ]),
+  ],
 })
 export class SidenavComponent implements OnInit {
   @Input() isMenuOpen: boolean;
-  @Input() contentMargin: number;
   constructor() {}
 
   ngOnInit() {}
